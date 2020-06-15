2bc132702c5509e67d9fdea8bc3fbdec1184781a
Roberto Rosin <rosin1408@gmail.com>
2020-06-14 22:09:48 -0300

    Change back to localStorage

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,8 @@
-import localforage from 'localforage';
-
 export const TOKEN_KEY = "@my-app-Token";
 
-export const isAuthenticated = async () => {
-  const token = await localforage.getItem(TOKEN_KEY);
-  return token !== null;
-};
+export const isAuthenticated = () => const token = localStorage.getItem(TOKEN_KEY) !== null;
 
-export const getToken = () => localforage.getItem(TOKEN_KEY).then(token => token);
+export const getToken = () => localStorage.getItem(TOKEN_KEY).then(token => token);
 
 export const login = token => localforage.setItem(TOKEN_KEY, token.accessToken)
 
